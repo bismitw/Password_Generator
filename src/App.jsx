48fe64dab45608c26af4d5bbfa0c2b79ef447130
Bismit Wagle <bismitwagle@gmail.com>
2026-01-14 18:04:52 +0530

    Add copy button logic and ui return

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState,useEffect } from 'react'
+import { useCallback, useState, useEffect, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,7 +8,7 @@ function App() {
   const [numberAllowed, setNumberAllowed] = useState(false)
   const [charAllowed, setCharAllowed] = useState(false)
   const [password, setPassword] = useState('')
-
+  const passwordRef = useRef(null)
   const generatePassword = useCallback(() => {
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
@@ -26,6 +26,10 @@ function App() {
     generatePassword()
   }, [length, numberAllowed, charAllowed])
 
+  const copyPasswordToClipboard= () =>{
+    window.navigator.clipboard.writeText(password);
+    passwordRef.current.select()
+  }
   return (
     <>
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
@@ -37,8 +41,11 @@ function App() {
             className="bg-white outline-none w-full py-1 px-3"
             placeholder="Password"
             readOnly
+            ref={passwordRef}
           />
-          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+          <button 
+          onClick={copyPasswordToClipboard}
+          className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
             copy
           </button>
         </div>
